db80a18ecbfbb22e3cde7723c7f9ee30f76a396a
우동진 <wjinh93@gmail.com>
2022-08-26 23:05:55 +0900

    [#7] feat: Head, Input 컴포넌트 구현

---
+++ client/components/Form.tsx
@@ -0,0 +1,10 @@
+import tw from "tailwind-styled-components";
+
+const Form = tw.form`
+  flex
+  flex-col
+  justify-center
+  items-center
+`;
+
+export default Form;
---
+++ client/components/Input.tsx
@@ -0,0 +1,15 @@
+import tw from "tailwind-styled-components";
+
+const Input = tw.input`
+    border
+    text-xs
+    rounded
+    border-black
+    p-2
+    mb-6
+    h-10
+    w-72
+    focus:outline-blue
+`;
+
+export default Input;
