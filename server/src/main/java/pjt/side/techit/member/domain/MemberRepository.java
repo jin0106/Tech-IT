4651dd4b1261f033edb83d926f16400a14562d3d
YJ Kwon <loling970124@gmail.com>
2022-08-31 21:17:48 +0900

    [#11] feat: MemberRepository 구현

---
+++ server/src/main/java/pjt/side/techit/member/domain/MemberRepository.java
@@ -0,0 +1,11 @@
+package pjt.side.techit.member.domain;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
