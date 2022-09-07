98cce20418f01025316cd37bfe1f3cf2cbf1cf06
우동진 <wjinh93@gmail.com>
2022-09-08 01:33:13 +0900

    [#7] feat: toaster 위치 변경

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,9 @@
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Footer } from "@components/index";
-import { Toaster } from "react-hot-toast";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,8 +11,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<RecoilRoot>
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
-				<Toaster />
 				<Footer />
+				<Toaster />
 			</QueryClientProvider>
 		</RecoilRoot>
 	);
