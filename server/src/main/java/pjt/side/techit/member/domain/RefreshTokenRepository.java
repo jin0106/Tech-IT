44b588a00c5c0fa44c1aa2ebb841ab3eed2b0e01
YJ Kwon <loling970124@gmail.com>
2022-09-07 17:07:54 +0900

    [#24] feat: RefreshTokenRepository 추가

---
+++ server/src/main/java/pjt/side/techit/member/domain/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package pjt.side.techit.member.domain;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findBySubject(String subject);
+}
