e6c9549db07f489b105bfd54b6ac907fe64d7813
YJ Kwon <loling970124@gmail.com>
2022-09-18 19:16:14 +0900

    [#34] feat: Token json camel case 변경

--- server/src/main/java/pjt/side/techit/member/controller/dto/response/TokenResponse.java
+++ server/src/main/java/pjt/side/techit/member/controller/dto/response/TokenResponse.java
@@ -1,16 +1,19 @@
 package pjt.side.techit.member.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class TokenResponse {
 
-    @JsonProperty("access_token")
+    @JsonProperty("accessToken")
+    @ApiModelProperty(notes = "액세스 토큰")
     private String accessToken;
 
-    @JsonProperty("refresh_token")
+    @JsonProperty("refreshToken")
+    @ApiModelProperty(notes = "리프레시 토큰")
     private String refreshToken;
 
     public TokenResponse() {
